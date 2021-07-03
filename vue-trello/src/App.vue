44bd178e6e7dfeb04a5cde9b618706bdca2c40c4
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-07-02 21:59:42 -0500

    Creacion del proyecto y primeros componentes

---
+++ vue-trello/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <app-eader></app-eader>
+    <router-view class="container"/>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppFooter from '@/components/AppFooter'
+import AppHeader from '@/components/AppHeader'
+
+export default {
+  components: { AppFooter, AppHeader },
+  name: 'App',
+}
+</script>
+
+<style scoped>
+#app {
+  box-sizing: border-box;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #37474f;
+  margin: 0 auto;
+  padding: 0;
+}
+
+.container {
+  padding-bottom: 4rem;
+}
+</style>
---
+++ vue-trello/src/components/AppFooter.vue
@@ -0,0 +1,31 @@
+<template>
+    <footer>
+        <span>
+            HI HI HI HI HI
+        </span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'app-footer',
+}
+</script>
+
+<style lang="scss" scoped>
+    footer{
+        box-sizing: border-box;
+        background-color: #cfd8dc;
+        bottom: 0;
+        left: 0;
+        padding: 1rem;
+        position: absolute;
+        width: 100%;
+
+        a {
+            text-decoration: none;
+            color: #546E7A;
+        }
+
+    }
+</style>
