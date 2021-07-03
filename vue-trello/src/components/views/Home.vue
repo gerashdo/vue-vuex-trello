b8479c408cdf5e4c1fc6c2afb56d2f169ec201ac
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-07-03 14:26:03 -0500

    Componente home

--- vue-trello/src/components/views/Home.vue
+++ vue-trello/src/components/views/Home.vue
@@ -1,9 +1,77 @@
 <template>
-    <h1>Home</h1>
+    <div>
+        <h3>Mis Paneles</h3>
+        <div class="boards-collection">
+            <input 
+            type="text"
+            placeholder="Añade un nuevo panel"
+            v-model="boardName"
+            @keyup.enter="add()"/>
+        <board-card
+            v-for="(board, index) in boards"
+            :key="index"
+            :name="board.name"
+            :id="board.id">
+            </board-card>
+        </div>
+    </div>
 </template>
 
 <script>
+import BoardCard from '@/components/BoardCard'
+
 export default {
-    name: 'home-view'
+    name: 'home-view',
+    components: { BoardCard },
+    data () {
+        return {
+            boardName: '',
+            boards: [
+                { id: 1, name: 'Tareas' },
+                { id: 2, name: 'Lista de la compra'}
+            ],
+        }
+    },
+    methods: { 
+        add () {
+            this.boards.push({ name: this.boardName })
+        }
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+    h3 {
+        text-align: left;
+        margin: 1rem;
+    }
+
+    .board-collection {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        padding: top;
+    }
+
+    input {
+        box-sizing: border-box;
+        background-color: #546E7A;
+        border: 2px solid #546E7A;
+        border-radius: 3px;
+        font-size: 1.1rem;
+        outline: 0;
+        padding: 0.5rem;
+        transition: all 600ms ease;
+
+        &:focus,
+        &:active {
+            background-color: white;
+            color: #546E7A;
+        }
+
+        &::placeholder {
+            color: white;
+        }
+    }
+</style>
