66f8fad6459b961fa52706912ba81ec74e537689
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-07-19 11:04:58 -0500

    Initial state

--- vue-trello/src/store/index.js
+++ vue-trello/src/store/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
 
+import state from './state'
+
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
@@ -9,7 +11,7 @@ const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
     strict: debug,
     plugins: debug ? [createLogger()] : [],
-    state: {},
+    state,
     getters: {},
     acctions: {},
     mutations: {}
