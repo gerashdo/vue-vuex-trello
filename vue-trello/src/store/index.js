bd63a26b49c52f9e21fa957cfa85aadf822dbc25
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-07-19 17:31:08 -0500

    Add getters file

--- vue-trello/src/store/index.js
+++ vue-trello/src/store/index.js
@@ -5,6 +5,7 @@ import createLogger from 'vuex/dist/logger'
 import state from './state'
 import mutations from './mutations'
 import actions from './actions'
+import getters from './getters'
 
 Vue.use(Vuex)
 
@@ -14,7 +15,7 @@ export default new Vuex.Store({
     strict: debug,
     plugins: debug ? [createLogger()] : [],
     state,
-    getters: {},
+    getters,
     actions,
     mutations
 })
